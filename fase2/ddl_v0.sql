c742fc64c8367862ca32932b3b82262030c13493
Pau24711 <merazpau24@gmail.com>
2026-09-24 18:29:25 +0000

    DDL v0: agregar producto pedido y detalle

--- fase2/ddl_v0.sql
+++ fase2/ddl_v0.sql
@@ -16,3 +16,32 @@ CREATE TABLE categoria (
     nombre VARCHAR(80) NOT NULL,
     descripcion VARCHAR(255)
 );
+
+
+CREATE TABLE producto (
+    id_producto INTEGER PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(255),
+    precio NUMERIC(10,2) NOT NULL,
+    stock INTEGER NOT NULL,
+    id_categoria INTEGER NOT NULL
+        REFERENCES categoria(id_categoria)
+);
+
+CREATE TABLE pedido (
+    id_pedido INTEGER PRIMARY KEY,
+    id_cliente INTEGER NOT NULL
+        REFERENCES cliente(id_cliente),
+    fecha_pedido DATE NOT NULL,
+    estado VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE detalle_pedido (
+    id_pedido INTEGER NOT NULL
+        REFERENCES pedido(id_pedido),
+    id_producto INTEGER NOT NULL
+        REFERENCES producto(id_producto),
+    cantidad INTEGER NOT NULL,
+    precio_unitario NUMERIC(10,2) NOT NULL,
+    PRIMARY KEY (id_pedido, id_producto)
+);
